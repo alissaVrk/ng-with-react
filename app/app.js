32d7e7420696b9fdcba22093cbe44b7425edfb07
Alissa <alissa.vrk@gmail.com>
2022-01-09 15:20:21 +0200

    load react by copy paste

--- app/app.js
+++ app/app.js
@@ -5,9 +5,10 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  'myApp.version',
+  'myApp.reactComps',
+])
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
